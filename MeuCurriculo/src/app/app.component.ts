a72f6a11b7786cc988abb98f9db0502d28f06995
Pedro Augusto <pedro.augusto@hotmail.com>
2025-11-17 23:40:11 -0300

    Adicionar componente Home, dicas e atualizar AppComponent

--- MeuCurriculo/src/app/app.component.ts
+++ MeuCurriculo/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DicasComponent } from './dicas/dicas.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, DicasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
